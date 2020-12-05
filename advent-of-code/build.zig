26d6e04083f2a201765f9ed300e42605fadfe1a7
hbobenicio <hbobenicio@gmail.com>
2020-12-05 12:52:55 -0300

    reorganizing day 01 and build.zig

--- advent-of-code/build.zig
+++ advent-of-code/build.zig
@@ -1,32 +1,20 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
+const Builder = std.build.Builder;
+const LibExeObjStep = std.build.LibExeObjStep;
 
 pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const day_one_exe = b.addExecutable("day-01", "src/01/main.zig");
-    day_one_exe.setTarget(target);
-    day_one_exe.setBuildMode(mode);
-    day_one_exe.install();
+    _ = addDay(b, target, mode, "day-01.1", "src/01/day-01.1.zig");
+    _ = addDay(b, target, mode, "day-01.2", "src/01/day-01.2.zig");
+}
 
-    const run_cmd = day_one_exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+fn addDay(b: *Builder, target: anytype, mode: std.builtin.Mode, target_name: []const u8, source: []const u8) *LibExeObjStep {
+    var exe = b.addExecutable(target_name, source);
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
 
-    const day_two_exe = b.addExecutable("day-02", "src/02/main.zig");
-    day_two_exe.setTarget(target);
-    day_two_exe.setBuildMode(mode);
-    day_two_exe.install();
-
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
+    return exe;
 }
