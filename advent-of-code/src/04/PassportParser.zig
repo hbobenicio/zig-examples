668d79886e1455c82892d065ed171286a9d4b43e
hbobenicio <hbobenicio@gmail.com>
2020-12-14 09:24:11 -0300

    advent of code: day 4 just minor advances... not working yet though

--- advent-of-code/src/04/PassportParser.zig
+++ advent-of-code/src/04/PassportParser.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
+const token = @import("./token.zig");
+const TokenKind = token.TokenKind;
+const Token = token.Token;
 const Tokenizer = @import("./Tokenizer.zig").Tokenizer;
 const Passport = @import("./Passport.zig").Passport;
 
@@ -10,6 +13,33 @@ pub const PassportParser = struct {
     input: []const u8,
     tokenizer: Tokenizer,
 
-    // AST
+    // Simplified AST
     passports: ArrayList(Passport),
+
+    pub fn init(alloc: *Allocator, input: []const u8) !PassportParser {
+        return PassportParser {
+            .alloc = alloc,
+            .input = input,
+            .tokenizer = try Tokenizer.init(alloc, input),
+            .passports = try ArrayList(Passport).init(alloc),
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.passports.deinit();
+        self.tokenizer.deinit();
+    }
+
+    pub fn parse(self: *@This()) !void {
+        try self.tokenizer.scan();
+
+        var token_offset: usize = 0;
+        var curr_token: Token = self.tokenizer.tokens.items[token_offset];
+        while (curr_token.kind != TokenKind.eof and token_offset < self.tokenizer.token.items.len) {
+            
+
+            token_offset += 1;
+            curr_token = self.tokenizer.tokens.items[token_offset];
+        }
+    }
 };
